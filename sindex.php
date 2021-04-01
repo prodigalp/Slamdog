2d79c0fa4f87a4236f7374b7b1e0b404b504b405
eyestrain <prodigalp@gmail.com>
2021-04-01 14:53:38 +0800

    Build a master branch contain index and style file

---
+++ sindex.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flexbox Procrastination</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <h1>Eyestrain Website</h1>
+        </header>
+        <section class="firstRow">
+            <div class="colOne">
+                <h2>Animation</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet maxime praesentium cumque optio eaque temporibus nobis et nihil odit nemo architecto provident soluta, eum aspernatur neque eveniet? Sint, eius.</p>
+            </div>
+            <div class="colTwo">
+                <h2>Dynamite</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus omnis quisquam nostrum maxime ex! Cupiditate quaerat magni, explicabo ipsam quam perspiciatis quidem debitis earum beatae perferendis error ipsa sequi incidunt.</p>
+            </div>
+            <div class="colTri">
+                <h2>Turn Table</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quia sed iusto, rerum libero vitae dolorum tenetur! Accusamus quisquam, enim, repellendus suscipit, mollitia necessitatibus quaerat inventore laudantium dolorem illo voluptas?</p>
+            </div>
+        </section>
+        <section class="secondRow">
+            <div class="colFour">
+                <h2>Villain</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, esse itaque, minima, quisquam dignissimos facere debitis ut odit ab quod magnam soluta tenetur totam molestiae dolore blanditiis laborum doloremque velit.</p>
+            </div>
+            <div class="colFive">
+                <h2>Atlantis</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, minus fugit dolore ducimus, quaerat fuga labore ipsum ullam nostrum quia tempore voluptates quam velit, optio culpa. Et pariatur delectus magnam.</p>
+            </div>
+        </section>
+        <footer>
+            <p>Copyright <?php echo date('Y'); ?> | Programmer : Karim Abdul Tapar</p>
+        </footer>
+    </div>
+</body>
+
+</html>
